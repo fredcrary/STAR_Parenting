0bff6ae5000805bccfa8cb1c30c34dd3b023544f
Jeremy Duke <jeremy@tetrapod.io>
2017-10-10 22:17:37 -0700

    rename gif files to meet convention, remove the fabs from the java as well.

--- android/StarParent/app/src/main/java/com/starparent/starparent/QuickIdeasMainActivity.java
+++ android/StarParent/app/src/main/java/com/starparent/starparent/QuickIdeasMainActivity.java
@@ -15,15 +15,6 @@ public class QuickIdeasMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quick_ideas_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
