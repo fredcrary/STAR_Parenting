b60f03efa1bfc7fa4c9054f4d6700dbdf453b51c
Jeremy Duke <jeremy@tetrapod.io>
2017-10-18 22:54:46 -0700

    Merge in first pass at hierarchy.  Add a temporary button to get back to Idea Bank so we can access it

--- android/StarParent/app/src/main/java/com/starparent/starparent/MainActivity.java
+++ android/StarParent/app/src/main/java/com/starparent/starparent/MainActivity.java
@@ -1,11 +1,13 @@
 package com.starparent.starparent;
 
+import android.text.Html;
 import android.util.Log;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -35,11 +37,28 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //Learn About Star Parenting Button
-        Button btn_idea_bank_main = (Button)findViewById(R.id.btn_learn_about_main);
-        btn_idea_bank_main.setOnClickListener(new View.OnClickListener() {
+        Button btn_learn_about_main = (Button)findViewById(R.id.btn_learn_about_main);
+        btn_learn_about_main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, LearnAboutStarMainActivity.class));
+            }
+        });
+
+        //Stuff below this line is only here temporarily for easy-access
+
+        StringBuilder htmlString = new StringBuilder();
+        htmlString.append("<h2>below here there be dragons</h2>");
+        htmlString.append("<hr>");
+        TextView textView = (TextView) findViewById(R.id.textViewMain);
+        textView.setText(Html.fromHtml(htmlString.toString()));
+
+        //Idea Bank Button (just for now to have an entry point
+        Button btn_idea_bank_main = (Button)findViewById(R.id.btn_idea_bank_main);
+        btn_idea_bank_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, IdeaBankMainActivity.class));
             }
         });
     }
