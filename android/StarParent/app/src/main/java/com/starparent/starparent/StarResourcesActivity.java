f1692278b727c5e726c7a9b8ab6ab764aecddc72
markpfaff <markdpfaff@gmail.com>
2017-11-03 19:20:20 -0700

    xml display update

--- android/StarParent/app/src/main/java/com/starparent/starparent/StarResourcesActivity.java
+++ android/StarParent/app/src/main/java/com/starparent/starparent/StarResourcesActivity.java
@@ -3,6 +3,8 @@ package com.starparent.starparent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -23,34 +25,17 @@ public class StarResourcesActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.textView);
         Log.d(TAG, "on create started");
 
-        //Bundle b = getIntent().getExtras();
-//        if (b.getSerializable("entry") != null) {
-//            entry = (StaticClasses.ResourceEntry) b.getSerializable("entry");
-//            Log.d(TAG, "if block getserialized");
-//
-//        }
-//        if (b.getSerializable("entry") != null) {
-//            name = (StaticClasses.ResourceEntry) b.getSerializable("name");
-//            Log.d(TAG, "if block getserialized");
-//
-//        }
-//        if (b.getSerializable("entry") != null) {
-//            url = (StaticClasses.ResourceEntry) b.getSerializable("url");
-//            Log.d(TAG, "if block getserialized");
-//
-//        }
-
         StringBuilder htmlString = new StringBuilder();
         htmlString.append("<p>" + entry + "</p>");
         htmlString.append("<p>" + name + "</p>");
         htmlString.append("<p>" + url + "</p>");
-//        htmlString.append("<p> test </p>");
-//        htmlString.append("<p> test </p>");
-//        htmlString.append("<p> test </p>");
-//        Log.d(TAG, String.valueOf(htmlString));
-//
-//        textView.setText(Html.fromHtml(htmlString.toString()));
-//        textView.setMovementMethod(new ScrollingMovementMethod());
+        htmlString.append("<p> test </p>");
+        htmlString.append("<p> test </p>");
+        htmlString.append("<p> test </p>");
+        Log.d(TAG, String.valueOf(htmlString));
+
+        textView.setText(Html.fromHtml(htmlString.toString()));
+        textView.setMovementMethod(new ScrollingMovementMethod());
 
     }
 
