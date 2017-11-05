c167329c4edc5ca1ccbdcf8075158bdbfb7de32c
markpfaff <markdpfaff@gmail.com>
2017-11-04 17:49:57 -0700

    adds initial problem solver activity

--- android/StarParent/app/src/main/java/com/starparent/starparent/ProblemSolverMainActivity.java
+++ android/StarParent/app/src/main/java/com/starparent/starparent/ProblemSolverMainActivity.java
@@ -1,21 +1,50 @@
 package com.starparent.starparent;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class ProblemSolverMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_learn_about_star);
+        setContentView(R.layout.activity_problem_solver);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Process Button
+        Button btn_quick_ideas = (Button)findViewById(R.id.btn_quick_ideas);
+        btn_quick_ideas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProblemSolverMainActivity.this, QuickIdeasMainActivity.class));
+            }
+        });
+
+        //Points Button
+        Button btn_idea_bank = (Button)findViewById(R.id.btn_idea_bank);
+        btn_idea_bank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProblemSolverMainActivity.this, IdeaBankMainActivity.class));
+            }
+        });
+
+        //Points Button
+        Button btn_problem_solving_guide = (Button)findViewById(R.id.btn_problem_solving_guide);
+        btn_problem_solving_guide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProblemSolverMainActivity.this, ProblemSolvingGuideActivity.class));
+            }
+        });
+
+
     }
 
 }
