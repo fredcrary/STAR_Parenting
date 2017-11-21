e9e796280586a35f7d3a44af44b81cc89c8b4f66
Mitch Thompson <thomitchell@gmail.com>
2017-11-21 13:36:31 -0800

    Adds expandable/collapsible views to problem solving guide.

---
+++ android/ExpandableLayout/app/src/main/java/com/example/mitch/expandablelayout/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.mitch.expandablelayout;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
+
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    ExpandableRelativeLayout expandableLayout1, expandableLayout2, expandableLayout3, expandableLayout4;
+    Toolbar toolbar;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolBar);
+        toolbar.setTitle("Expandable Layout Tutorial");
+
+    }
+
+    public void expandableButton1(View view) {
+        expandableLayout1 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout1);
+        expandableLayout1.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButton2(View view) {
+        expandableLayout2 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout2);
+        expandableLayout2.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButton3(View view) {
+        expandableLayout3 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout3);
+        expandableLayout3.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButton4(View view) {
+        expandableLayout4 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout4);
+        expandableLayout4.toggle(); // toggle expand and collapse
+    }
+}
--- android/StarParent/app/src/main/java/com/starparent/starparent/ProblemSolvingGuideActivity.java
+++ android/StarParent/app/src/main/java/com/starparent/starparent/ProblemSolvingGuideActivity.java
@@ -1,43 +1,42 @@
 package com.starparent.starparent;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import net.cachapa.expandablelayout.ExpandableLayout;
 
 public class ProblemSolvingGuideActivity extends BaseNavigationDrawerActivity {
+
+    ExpandableLayout expandableLayout1, expandableLayout2, expandableLayout3, expandableLayout4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         onCreateDrawer();
-        getLayoutInflater().inflate(R.layout.activity_problem_solving_guide, frameLayout);
+        setContentView(R.layout.activity_problem_solving_guide);
+        //getLayoutInflater().inflate(R.layout.activity_problem_solving_guide, frameLayout);
         setTitle("Problem Solving Guide");
 
-        //save button
-        Button ps_guide_save_btn = (Button)findViewById(R.id.ps_guide_save_btn);
-        ps_guide_save_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //display in long period of time
-                Toast.makeText(getApplicationContext(), "Form has been saved",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
+    }
 
-        //next button
-        Button ps_guide_next_btn = (Button)findViewById(R.id.ps_guide_next_btn);
-        ps_guide_next_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(ProblemSolvingGuideActivity.this, PS_Guide_StopFocus.class));
-            }
-        });
+    public void expandableButton1(View view) {
+        expandableLayout1 = (ExpandableLayout) findViewById(R.id.expandableLayout1);
+        expandableLayout1.toggle(); // toggle expand and collapse
+
+    }
+
+    public void expandableButton2(View view) {
+        expandableLayout2 = (ExpandableLayout) findViewById(R.id.expandableLayout2);
+        expandableLayout2.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButton3(View view) {
+        expandableLayout3 = (ExpandableLayout) findViewById(R.id.expandableLayout3);
+        expandableLayout3.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButton4(View view) {
+        expandableLayout4 = (ExpandableLayout) findViewById(R.id.expandableLayout4);
+        expandableLayout4.toggle(); // toggle expand and collapse
     }
 
 }
