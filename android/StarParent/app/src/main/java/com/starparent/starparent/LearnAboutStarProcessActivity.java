bf1e93173dc7545cf28ab383b278d5760fa8eda5
markpfaff <markdpfaff@gmail.com>
2017-12-11 19:17:19 -0800

    style&layout updates

--- android/StarParent/app/src/main/java/com/starparent/starparent/LearnAboutStarProcessActivity.java
+++ android/StarParent/app/src/main/java/com/starparent/starparent/LearnAboutStarProcessActivity.java
@@ -3,6 +3,7 @@ package com.starparent.starparent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class LearnAboutStarProcessActivity extends BaseNavigationDrawerActivity {
@@ -16,13 +17,22 @@ public class LearnAboutStarProcessActivity extends BaseNavigationDrawerActivity 
         getLayoutInflater().inflate(R.layout.activity_learn_about_star_process, frameLayout);
         setTitle("STAR Process");
 
-        //Star Process ImageButton
-        learn_about_process_imageBtn = (ImageButton) findViewById(R.id.learn_about_process_imageButton);
-        learn_about_process_imageBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
+        //Create buttons and onClick listeners
+        Button btn_star_process =(Button)findViewById(R.id.btn_star_process);
+        btn_star_process.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity(new Intent(LearnAboutStarProcessActivity.this, MainActivity.class));
+                Intent ii = new Intent(LearnAboutStarProcessActivity.this, MainActivity.class);
+                startActivity(ii);
             }
         });
+
+        //Star Process ImageButton
+        learn_about_process_imageBtn = (ImageButton) findViewById(R.id.learn_about_process_imageButton);
+//        learn_about_process_imageBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(LearnAboutStarProcessActivity.this, MainActivity.class));
+//            }
+//        });
     }
 }
